6c42031e7ebb531f3b6dde451c935814e1c36514
Ashkan Abedi <ashkan.gplay89@gmail.com>
2025-09-28 17:41:18 +0200

    Add inital files and folders

---
+++ Source/legotool_plugin/legotool_plugin.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "legotool_plugin.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, legotool_plugin, "legotool_plugin" );
