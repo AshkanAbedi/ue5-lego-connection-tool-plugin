e0fcfadf6c183d4a5150fb5857af4b16488f2ca2
Ashkan Abedi <ashkan.gplay89@gmail.com>
2025-10-03 09:18:40 +0200

    Rename EShapeType to EShapeTypes and update references in LegoActor and LegoLevelSerializer

--- Plugins/LegoConnectionTool/Source/LegoConnectionTool/Public/LegoLevelSerializer.h
+++ Plugins/LegoConnectionTool/Source/LegoConnectionTool/Public/LegoLevelSerializer.h
@@ -1,7 +1,7 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 #pragma once
 #include "CoreMinimal.h"
-#include "EShapeType.h"
+#include "EShapeTypes.h"
 #include "UObject/Object.h"
 #include "LegoLevelSerializer.generated.h"
 
